0042c6132031a70db3412c327f628954391c8fc5
Preben Øhre <prebenohre@gmail.com>
2025-02-02 14:25:55 +0100

    Fixed minor issues with api

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
-// Importerer stilark og JSConfetti-modulen.
 import "./style.css";
 import JSConfetti from "js-confetti";
 
-// Velger form-elementet og textarea-elementet fra dokumentet.
 const form = document.querySelector("form");
 const textArea = document.querySelector("textarea");
 const jsConfetti = new JSConfetti();
 
-// Oppretter en ny instans av JSConfetti.
 form.addEventListener("submit", handleSubmit);
 
-// Legger til en event listener for å håndtere 'keypress'-hendelsen på textarea.
-// Denne sjekker om brukeren trykker på Enter-tasten.
+// Ny kode for å håndtere Enter-tasten
 textArea.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    event.preventDefault(); // // Forhindrer standard oppførsel (ny linje).
-    handleSubmit(event); // Kaller handleSubmit-funksjonen.
+    event.preventDefault(); // Forhindrer ny linje i textarea
+    handleSubmit(event); // Kaller handleSubmit-funksjonen
   }
 });
 
-// Definerer funksjonen som håndterer innsending av skjemaet.
 async function handleSubmit(e) {
-  e.preventDefault(); // Forhindrer skjemaet i å sende en HTTP-forespørsel på vanlig måte.
-  showSpinner(); // Viser en spinner på knappen for å indikere prosessering.
-  const prompt = textArea.value; // Henter innholdet av textarea.
+  e.preventDefault();
+  showSpinner();
+  const prompt = textArea.value;
 
-  // Sender en POST-forespørsel til serveren.
   const response = await fetch(
     "https://ai-photo-generator-0108636d5bc8.herokuapp.com/",
     {
@@ -38,36 +32,31 @@ async function handleSubmit(e) {
   );
 
   if (response.ok) {
-    // Behandler svaret fra serveren hvis forespørselen var vellykket.
     const { image } = await response.json();
     const result = document.querySelector("#result");
     result.innerHTML = `<img src="${image}" width="512" />`;
-    triggerConfetti(); // Utløser konfetti-animasjon.
+    triggerConfetti(); // Kaller konfetti her
   } else {
-    // Håndterer feil fra serveren.
     const err = await response.text();
     alert(err);
     console.error(err);
   }
 
-  hideSpinner(); // Skjuler spinneren.
+  hideSpinner();
 }
 
-// Viser en spinner på genereringsknappen.
 function showSpinner() {
   const button = document.querySelector("button");
   button.disabled = true;
   button.innerHTML = 'GENERATING... <span class="spinner">✨</span>';
 }
 
-// Skjuler spinneren og gjenaktiverer knappen.
 function hideSpinner() {
   const button = document.querySelector("button");
   button.disabled = false;
   button.innerHTML = "GENERATE";
 }
 
-// Definerer funksjonen for å utløse konfetti-animasjonen.
 function triggerConfetti() {
   jsConfetti.addConfetti({
     emojis: [
@@ -84,7 +73,7 @@ function triggerConfetti() {
       "😍",
       "💫",
     ],
-    emojiSize: 50, // Størrelsen på emojiene i konfetti-animasjonen.
-    confettiNumber: 150, // Antall konfettibiter som skal vises.
+    emojiSize: 50,
+    confettiNumber: 150,
   });
 }
--- server.js
+++ server.js
@@ -4,15 +4,12 @@ import OpenAI from "openai";
 import express from "express";
 import cors from "cors";
 
-// Laster inn miljøvariabler fra .env-filen i utviklingsmiljøet
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
-// Initialiserer en ny Express-applikasjon
 const app = express();
 
-// Omdirigerer alle HTTP-forespørsler til HTTPS i produksjonsmiljøet
 app.use((req, res, next) => {
   if (
     req.header("x-forwarded-proto") !== "https" &&
@@ -24,50 +21,31 @@ app.use((req, res, next) => {
   }
 });
 
-// Aktiverer CORS (Cross-Origin Resource Sharing) for å tillate forespørsler fra forskjellige domener.
-app.use(
-  cors({
-    origin: "*", // Tillater alle domener (kan begrense dette i prod)
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  }),
-);
-
-// Gjør det mulig for Express å tolke JSON i innkommende forespørsler.
+app.use(cors());
 app.use(express.json());
 
-// Initialiserer OpenAI-API-klienten med API-nøkkelen.
 const openai = new OpenAI({
   apiKey: process.env.OPENAI,
 });
 
-// Definerer en rute for POST-forespørsler til rot-URL-en.
 app.post("/", async (req, res) => {
   try {
-    // Henter prompt-verdien.
     const prompt = req.body.prompt;
-
-    // Genererer et bilde basert på prompten ved hjelp av OpenAI API.
     const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
     });
-
-    // Logger svaret fra OpenAI API.
     console.log(aiResponse);
-
-    // Sender bildets URL som respons.
     const image = aiResponse.data[0].url;
     res.send({ image });
   } catch (err) {
-    // Logger og sender eventuelle feilmeldinger.
     console.error(err);
     res.status(500).send(err.message);
   }
 });
 
-// Omdirigerer alle GET-forespørsler til index.html, for å støtte single-page applikasjoner.
+// Tjener de statiske filene som Vite har bygget i produksjonsmodus
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("dist"));
 
@@ -76,6 +54,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Starter serveren på en spesifikk port (hentet fra miljøvariablene, eller 8080 som standard).
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
